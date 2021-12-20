f0e8af952851f423bbea81d4c9f451f9e86d592d
Vadym Bezdushnyi <vadym.bezdushnyi@ringteam.com>
2021-12-20 02:12:43 +0200

    Fix for heroku

--- wsgi.py
+++ wsgi.py
@@ -3,4 +3,5 @@ import os
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(port=port)
+    print("Running from wsgi on", port)
+    app.run(debug=True, host='0.0.0.0')
